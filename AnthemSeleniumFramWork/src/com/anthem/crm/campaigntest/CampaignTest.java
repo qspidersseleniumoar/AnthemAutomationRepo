55e09e7fc5c856b18765661297ee1314439e15a4
Madhusudhan M <Madhusudhan M@LAPTOP-H02BB1TP>
2019-10-12 14:32:13 +0530

    my commit to local repo

---
+++ AnthemSeleniumFramWork/src/com/anthem/crm/campaigntest/CampaignTest.java
@@ -0,0 +1,9 @@
+package com.anthem.crm.campaigntest;
+
+public class CampaignTest {
+	
+	public void createCampaignTest() {
+		System.out.println("execute");
+	}
+
+}
---
+++ AnthemSeleniumFramWork/src/com/anthem/crm/genericlib/BaseTest.java
@@ -0,0 +1,5 @@
+package com.anthem.crm.genericlib;
+
+public class BaseTest {
+
+}
